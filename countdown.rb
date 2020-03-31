3b26058544a61a2e242474f1f73d0b4b5279fa58
Megan Gomes <megan.gomes25@gmail.com>
2020-03-31 01:26:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,12 @@ def countdown(integer)
 end
 
 
+def countdown_with_sleep(integer)
+
+  while (integer > 0)
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+    integer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
